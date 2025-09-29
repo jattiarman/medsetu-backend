999ff861d75c2505628b6aa5e5fadbc8603738b3
Arman M Jatti <123095283+jattiarman@users.noreply.github.com>
2025-09-29 21:17:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: 'https://namaste01.netlify.app/', // Your live frontend URL
+  optionsSuccessStatus: 200 
+};
+app.use(cors(corsOptions));
 app.use(cors());
 app.use(express.json());
 
